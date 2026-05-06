d8e8b35bcead4efd2a379cc9c77621be73e00a7c
Hamdan-Mosad <hmdanmosadkhter@gmail.com>
2026-05-06 14:35:04 +0300

    Add files via upload

---
+++ pro25from2.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int ReadRandomNumber(int from, int to) {
+    int randomnumber = rand() % (to - from + 1) + from;
+    return randomnumber;
+}
+void fillarraywithrandomnumber(int arr[100], int& arrlength) {
+    cout << "\nplease enter of element\n";
+    cin >> arrlength;
+    for (int i = 0;i < arrlength;i++) {
+        arr[i] = ReadRandomNumber(1, 100);
+    }
+}
+void Printarray(int arr[100], int arrlength) {
+    for (int i = 0;i < arrlength;i++) {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+}
+int minnumberinarray(int arr[100], int arrlength) {
+    int min= 0;
+    min = arr[0];
+    for (int i = 0;i < arrlength;i++) {
+        if (arr[i] < min) {
+            min = arr[i];
+        }
+    }
+    return min;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr[100], arrlength;
+    fillarraywithrandomnumber(arr, arrlength);
+    cout << "\narray element :\n";
+    Printarray(arr, arrlength);
+    cout << "\nmax number is:";
+    cout << minnumberinarray(arr, arrlength);
+    return 0;
+}
