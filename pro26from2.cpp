8657c439a42b936b3105377b248ed3b24bf967e2
Hamdan-Mosad <hmdanmosadkhter@gmail.com>
2026-05-07 06:52:15 +0300

    Add files via upload

---
+++ pro26from2.cpp
@@ -0,0 +1,42 @@
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int ReadRandomNumber(int from, int to) {
+    int randomnumber = rand() % (to - from + 1) + from;
+    return randomnumber;
+}
+void fillarraywithrandomnumber(int arr[100], int& arrlength) {
+    cout << "\nplease enter of element\n";
+    cin >> arrlength;
+    for (int i = 0;i < arrlength;i++) {
+        arr[i] = ReadRandomNumber(1, 100);
+    }
+}
+void Printarray(int arr[100], int arrlength) {
+    for (int i = 0;i < arrlength;i++) {
+        cout << arr[i] <<" ";
+    }
+    cout << endl;
+}
+int sumnumberinarray(int arr[100], int arrlength) {
+    int sum = 0;
+    for (int i = 0;i < arrlength;i++) {
+        
+            sum += arr[i];
+        
+    }
+    return sum;
+}
+int main()
+{
+    srand((unsigned)time(NULL));
+    int arr[100], arrlength;
+    fillarraywithrandomnumber(arr, arrlength);
+    cout << "\narray element :\n";
+    Printarray(arr, arrlength);
+    cout << "\nsum number in array:";
+    cout << sumnumberinarray(arr, arrlength);
+    return 0;
+}
